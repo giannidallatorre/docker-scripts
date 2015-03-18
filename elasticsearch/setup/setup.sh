1088154336962bee7f3600e554ba94d3b14af342
root <root@cloud-vm131.novalocal>
2015-03-18 16:24:05 +0100

    Temporary branch redirection

--- elasticsearch/setup/setup.sh
+++ elasticsearch/setup/setup.sh
@@ -2,8 +2,8 @@
 set -ex
 
 ## Run puppet
-git clone git://github.com/cnaf/ci-puppet-modules.git /ci-puppet-modules
-puppet apply --modulepath=/ci-puppet-modules/modules:/etc/puppet/modules/ /manifest-elasticsearch.pp
+git clone -b elasticsearch git://github.com/giannidallatorre/ci-puppet-modules.git /ci-puppet-elastic
+puppet apply --modulepath=/ci-puppet-modules/modules:/etc/puppet/modules:/ci-puppet-elastic/modules/ /manifest-elasticsearch.pp
 
 ## Install minimal utils
 #yum -y install nc mysql $JAVA_PACKAGE
